28fcb16ec9248c496c059d6f1c42c6a697f9c489
Eric Van Dornshuld <60555549+edornshuld@users.noreply.github.com>
2021-09-10 13:58:05 -0500

    Rebuild public/

--- ocs-4.3/public/find-link.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/csh
-
-foreach file ( `find ./ -type f \( -iname \*.md -o -iname \*.Rmd -iname \*.Rmarkdown \) `)
-    set num_links = `grep "dornshuld.chemistry.msstate.edu" $file | wc -l`
-    if ( $num_links > 0 ) then
-        echo $file
-    endif 
-end
-
